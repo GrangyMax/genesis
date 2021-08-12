ca1554e0d119035f7054bbf73d83ee8760e3bc08
Sergei Peshkov <peshkovsergei@gmail.com>
2021-08-12 16:49:22 +0300

    мелкие правки форм обратной связи

--- single-klinika.php
+++ single-klinika.php
@@ -253,7 +253,7 @@ if (have_posts()) : while (have_posts()) : the_post();
 					var form_clink = document.getElementById('template-contactform-submit');
 					form_clink.onclick = function() {				
 					$clinicID = $('.clinicId').attr('data-attr');					
-					ym(39014465, 'reachGoal', $clinicID); return true;	
+					ym(39014465, 'reachGoal', $clinicID); return true;
 				}			
 				
 			</script>
